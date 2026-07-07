3052822bf8ea07b63ea6d589cefd201758d3a1b3
William Hleucka <william.hleucka@gmail.com>
2026-07-07 11:40:07 -0600

    fix: alias response home controller

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use Hydra\Http\Attributes\Route;
-use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ResponseInterface as Response;
 
 final class HomeController extends Controller
 {
     #[Route('/')]
-    public function index(): ResponseInterface
+    public function index(): Response
     {
         return $this->render('home');
     }
