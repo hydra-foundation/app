b9d96feef8220c00b678de84ad7bed47b86cdeae
William Hleucka <william.hleucka@gmail.com>
2026-09-10 12:11:01 -0600

    fix: mobile nav

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -2,6 +2,8 @@
 <?php /** @var \Hydra\Admin\ViewModels\ScreenViewModel $screen */ ?>
 <?php $this->extends('layouts/base') ?>
 
-<?php $this->start('meta') ?><link rel="stylesheet" href="/css/admin.css" /><?php $this->stop() ?>
+<?php /* The admin's own sheet and script, loaded only on admin screens. Both
+   travel with admin.css if it ever moves into the package. */ ?>
+<?php $this->start('meta') ?><link rel="stylesheet" href="/css/admin.css" /><script src="/js/admin.js" defer></script><?php $this->stop() ?>
 
 <?= $this->partial('admin/partials/shell', ['screen' => $screen, 'content' => $this->section('content')]) ?>
