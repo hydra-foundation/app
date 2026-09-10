87bd09b189057aa94a0b8dbf352ade32bed68040
William Hleucka <william.hleucka@gmail.com>
2026-09-10 09:51:42 -0600

    fix: update admin layout

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -2,7 +2,4 @@
 <?php /** @var \Hydra\Admin\ViewModels\ScreenViewModel $screen */ ?>
 <?php $this->extends('layouts/base') ?>
 
-<div class="admin d-flex align-items-stretch">
-    <?= $this->partial('admin/partials/sidebar', ['screen' => $screen]) ?>
-    <div id="admin-frame" class="admin-frame flex-grow-1 p-4"><?= $this->section('content') ?></div>
-</div>
+<?= $this->partial('admin/partials/shell', ['screen' => $screen, 'content' => $this->section('content')]) ?>
